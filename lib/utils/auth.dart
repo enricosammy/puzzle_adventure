e103f5e2e7104a4f864690f30e172015c20cd909
enricosammy <enrico.sammy@gmail.com>
2025-03-30 00:59:21 +0000

    Update auth.dart

--- lib/utils/auth.dart
+++ lib/utils/auth.dart
@@ -1,1 +1,18 @@
 
+import 'package:http/http.dart' as http;
+
+class Auth {
+  static Future<String> getApiToken() async {
+    // Logic to securely retrieve the token
+    // Example: Use flutter_secure_storage or environment variables to get the token
+    return 'replace_with_secure_token_logic';
+  }
+
+  static Future<http.Response> getAuthorizedRequest(String url) async {
+    final token = await getApiToken();
+    return http.get(
+      Uri.parse(url),
+      headers: {'Authorization': 'Bearer $token'},
+    );
+  }
+}
