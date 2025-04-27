63e1fd3b5093465cd66148a0eaccaf78bf5a5931
enricosammy <enrico.sammy@gmail.com>
2025-04-27 13:09:09 +0100

    Update image_reveal.dart

--- lib/features/image_reveal.dart
+++ lib/features/image_reveal.dart
@@ -1,1 +1,8 @@
-//test
+import '../config/flavor_config.dart';
+
+// List of flavors that should have access to the feature
+const List<String> allowedFlavors = ['women_puzzle'];
+
+bool isFeatureEnabled() {
+  return allowedFlavors.contains(FlavorConfig.instance!.flavor.name);
+}
